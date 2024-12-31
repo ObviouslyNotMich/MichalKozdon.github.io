4097fd320125d79a956caafabbba3c46eee75288
MichalKozdon <michal.kozdon@outlook.com>
2024-12-31 20:36:58 +0100

    css update

---
+++ js/load-navbar.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Find the element where we want to inject the nav
+    const navbarContainer = document.getElementById('navbar-container');
+    if (!navbarContainer) return;
+    
+    // Fetch the navbar.html file
+    fetch('../components/navbar.html')
+        .then(response => response.text())
+        .then(data => {
+            // Insert the HTML into the container
+            navbarContainer.innerHTML = data;
+        })
+        .catch(err => console.error('Error loading navbar:', err));
+});
+
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './components/**/*.{html,js}',
+    './pages/**/*.{html,js}',
+    './js/**/*.{html,js}',
+    './css/**/*.{html,js}',
+    './index.html',
+  ],
+  // ...
+}
